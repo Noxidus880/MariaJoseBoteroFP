c2033f7734bb2bbe798ab41f927df78099055a61
Noxidus880 <maria.boteroc@upb.edu.co>
2025-08-01 15:23:45 -0500

    Variables,constantes y tipos de datos.

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Esta es la declaracion e inicializacion de una variable
+            string nombre = "MariaJose";
+            const string iva = "19%";
+
+
+            Console.Write("Hola, " + nombre + "\n");
+            Console.Write("\tBienvenido a la clase de programacion");
+           // iva = "20%" ; No se puede cambiar el valor de una constante
+
+            // Tipos de datos
+            byte dato1 = 255;
+            int dato2 = 202321;
+            long dato3 = -943882934899;
+            double dato4 = 56.894;
+            char dato5 = '$';//puede almacenar un solo caracter 
+            bool dato6 = false;//Almacena true o false 
+            object dato7 = new Program();
+
+           
         }
     }
 }
--- 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfaa148228166b7eba466b760e50088686d32c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
