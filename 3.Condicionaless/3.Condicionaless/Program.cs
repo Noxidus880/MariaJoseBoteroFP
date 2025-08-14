cd581619b12b2bf69e4aed238f374f9d8364cbc1
Noxidus880 <maria.boteroc@upb.edu.co>
2025-08-13 20:19:48 -0500

    3.Condicionales

---
+++ 3.Condicionaless/3.Condicionaless/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _3.Condicionaless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Condicional simple 
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine(); // Todo lo que lea la consola va a
+                                         // llegar como string
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            { 
+                Console.WriteLine($"La persona {nombre} debe abonar impuestos");
+
+            }
+                                          
+
+        }
+    }
+}
