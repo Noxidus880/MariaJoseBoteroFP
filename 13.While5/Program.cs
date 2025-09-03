9ea43dd553a3fa07ddcd19d06fc02d7126a103d0
Noxidus880 <maria.boteroc@upb.edu.co>
2025-09-03 14:48:14 -0500

    Add files via upload

---
+++ 13.While5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _13.While5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.
+            int contador = 1;
+            int cantidadPares = 0;
+            int rango = 0;
+
+            Console.WriteLine("Ingrese el rango de números a evaluar:");
+            rango=int.Parse(Console.ReadLine());
+
+            while(contador<=rango)
+            {
+                if(contador%2==0)
+                {
+                    cantidadPares++;
+                }
+                contador++;
+
+            }
+            Console.WriteLine($"La cantidad de números pares del 1 hasta {rango},es:{cantidadPares}");
+
+
+
+
+
+        }
+    }
+}
