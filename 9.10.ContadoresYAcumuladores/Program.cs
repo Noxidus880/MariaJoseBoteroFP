79b10c144b2a7378a6b341e76cb1d50ac70010c2
Noxidus880 <maria.boteroc@upb.edu.co>
2025-09-03 15:04:38 -0500

    Add files via upload

---
+++ 9.10.ContadoresYAcumuladores/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _9._10.ContadoresYAcumuladores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escribir un algoritmo que permita generar primeros 5 números enteros positivos y realizar y mostrar su suma.
+        }
+
+
+
+
+       /* Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)*/
+
+
+
+
+
+
+
+    }
+
+}
