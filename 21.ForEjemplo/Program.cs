9b9a7630d292b62f7644899636e58dfbadb25aff
Noxidus880 <maria.boteroc@upb.edu.co>
2025-09-12 15:37:08 -0500

    Add files via upload

---
+++ 21.ForEjemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _21.ForEjemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for(int i=1;i<=5;i++)
+            {
+                acumulador += i;
+            }
+            Console.WriteLine($"La suma de los primeros cinco números enteros es {acumulador}");
+        }
+    }
+}
