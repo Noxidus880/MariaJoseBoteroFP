fd92f6f39e37145ddee85189627940fdadd98619
Noxidus880 <maria.boteroc@upb.edu.co>
2025-09-17 15:33:37 -0500

    Add files via upload

---
+++ 23.EjerciciosVectores/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _23.EjerciciosVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear arreglo llamado "numeros" de 100 elementos
+            //asignar el número 10 en cada una de las posiciones del arreglo. Leer el contenido de
+            //cada elemento y escribirlo en pantalla
+            int[] numeros;
+
+
+
+        }
+    }
+}
