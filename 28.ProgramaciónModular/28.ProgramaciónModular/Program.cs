3c674302697dc15ff2c3a7648d9277a671f8d068
Noxidus880 <maria.boteroc@upb.edu.co>
2025-10-04 18:00:56 -0500

    .

---
+++ 28.ProgramaciónModular/28.ProgramaciónModular/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _28.ProgramaciónModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Soy María José,");
+            MostrarMensajes($"tengo {CalcularEdad()} años.");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2005)} años.");
+        }
+
+        //Módulo 1 - Procedimiento sin parámetros
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("¡Bienvenid@ a la Programación Modular! =)");
+            MostrarMensajes("Estoy siendo llamado desde MostrarMensaje()");
+        }
+
+        //Módulo 2 - Procedimiento con parámetros
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Módulo 3 - Función sin parámetros
+
+        static int CalcularEdad()
+        {
+            int edad = 2025 - 2000;
+            return edad;
+        }
+
+        //Módulo 4 - Función con parámetros
+        static int CalcularEdad(int añoActual, int añoNacimiento)//Sobrecarga de métodos con mismo nombre. Diferencia: Parámetro(s) y tipo de dato que devuelven
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
---
+++ 29.ProgramaciónModular1/29.ProgramaciónModular1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _29.ProgramaciónModular1
+{
+    internal class Program
+    {
+        //29. Crear una matriz de n filas por m columnas, que permita ingresar solamente múltiplos de 5 hasta que se llene la matriz.
+
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarFilas(), CapturarColumnas()];
+        }
+
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el número de filas para la matriz:");
+            int filas = int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el número de columnas para la matriz:");
+            int columnas = int.Parse(Console.ReadLine());
+            return columnas;
+        }
+
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+
+            return matriz;
+        }
+    }
+}
+
